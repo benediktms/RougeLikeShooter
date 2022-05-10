144695e04a4736d6f82e4c0bfa27261585886ed1
James Schiele <jamesschiele@hotmail.com>
2022-05-10 22:42:36 +0100

    changed method name, one sway for moving camera, one sway for moving player, added starting position for weapon as weaponParentOrigin

--- Assets/Scripts3D/Weapons/Weapon.cs
+++ Assets/Scripts3D/Weapons/Weapon.cs
@@ -21,4 +21,38 @@ public abstract class Weapon : MonoBehaviour
     protected float ShootForce;
 
     protected abstract void Fire();
+    [Header("Bob Settings")]
+    private Vector3 _weaponOrigin;
+
+    private void Start()
+    {
+        _weaponOrigin = transform.localPosition;
+    }
+
+    private void Update()
+    {
+        RotationSway();
+    }
+
+    public void PlayMuzzleFlash()
+    {
+        MuzzleFlash.Play();
+    }
+
+    void RotationSway()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * _swayMultiplier;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * _swayMultiplier;
+
+        Quaternion rotationX = Quaternion.AngleAxis(-mouseY, Vector3.right);
+        Quaternion rotationY = Quaternion.AngleAxis(mouseX, Vector3.up);
+
+        Quaternion targetRotation = rotationX * rotationY;
+
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, _smooth * Time.deltaTime);
+    }
+
+    void HeadBob()
+    {
+    }
 }
