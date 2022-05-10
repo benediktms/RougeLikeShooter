b398e778a053f9902ff6a5d9ab00f82b674ec858
James Schiele <jamesschiele@hotmail.com>
2022-05-11 00:12:44 +0100

    refactor

--- Assets/Scripts3D/Weapons/Weapon.cs
+++ Assets/Scripts3D/Weapons/Weapon.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-
     [SerializeField] Camera FPCamera;
     [SerializeField] float Range = 100f;
     [SerializeField] float damage = 20f;
